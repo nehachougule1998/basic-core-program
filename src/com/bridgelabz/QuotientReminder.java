7e1be6a94dd34e4b096045092a4ad463bf9371e4
nehachougule1998 <nchougule734@gmail.com>
2022-03-05 22:20:57 +0530

    Quotient and Reminder

---
+++ src/com/bridgelabz/QuotientReminder.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+import java.util.Scanner;
+public class QuotientReminder {
+
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		System.out.print("Enter dividend: ");
+		int divident = num.nextInt();
+
+		System.out.print("Enter divisor: ");
+		int dsor = num.nextInt();
+		
+		System.out.println("Quotient is : " + divident / dsor);
+		System.out.println("Remainder is : " + divident % dsor);
+       }
+
+	}
+
+//number by which a dividend is divided
