3eb915481cfe4812de54fa65d7f471bb02012c59
nehachougule1998 <nchougule734@gmail.com>
2022-03-05 21:46:52 +0530

    check num even or odd

---
+++ src/com/bridgelabz/EvenandOdd.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+import java.util.Scanner;
+public class EvenandOdd {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in); 
+
+        System.out.print("Enter a number : ");
+
+        int num = read.nextInt(); 
+
+        if (num % 2 == 0) {   // using % operator check if it is divisible by 2 or not.
+
+            System.out.println(num + " is even");
+
+        } else {
+
+            System.out.println(num + " is odd");
+        }
+    }
+    
+ }
